93df89696f34d3951912febfcc3880630d9b1fa7
Prasad2098 <prasadparshallu2098@gmail.com>
2018-10-10 14:48:33 +0530

    Update service_data.pl

--- service-logging/service_data.pl
+++ service-logging/service_data.pl
@@ -1,5 +1,5 @@
-service_mon(acpid,active).
-service_mon(bluetooth,active).
+service_mon(acpid,active,vm1).
+service_mon(bluetooth,active,vm2).
 
 service(acpid).
 service(bluetooth).
@@ -7,15 +7,17 @@ service(bluetooth).
 ser_state(active).
 ser_state(inactive).
 
-ser_provided(acpid,[active]).
-ser_provided(bluetooth,[inactive]).
+ser_provided(acpid,[active],[vm1]).
+ser_provided(bluetooth,[active],[vm1,vm2]).
 
 ser_check(X,[X|_]).
 ser_check(X,[_|Tail]):- ser_check(X,Tail).
 
-ser_granted(X,Y):- ser_provided(X,Z),ser_check(Y,Z).
+vm_check(X,[X|_]).
+vm_check(X,[_|Tail]):- ser_check(X,Tail).
 
-check(X,Y):-service(X),ser_state(Y),ser_granted(X,Y).
+ser_granted(X,Y,V):- ser_provided(X,Z,K),ser_check(Y,Z),vm_check(V,K).
 
-sla():- forall(service_mon(X,Y),check(X,Y)).
+check(X,Y,V):-service(X),ser_state(Y),ser_granted(X,Y,V).
 
+sla():- forall(service_mon(X,Y,V),check(X,Y,V)).
