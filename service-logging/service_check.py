cae1205f842ecc62fca867ef4bd42e3892aaf1d7
Prasad2098 <prasadparshallu2098@gmail.com>
2018-10-10 13:37:48 +0530

    Add files via upload

---
+++ service-logging/service_check.py
@@ -0,0 +1,60 @@
+import os 
+import re
+
+m1 = {}
+m2 = {}
+
+
+output_file = open("/home/prasad/Desktop/btp/test2/service_data.pl","r")
+
+for line in output_file:
+	if 'service_mon' in line:
+		name = line.split(',')
+		ser_name = name[0].split('(')
+		ser_state = name[1].split(')')
+		if ser_name[1] in m1:		
+			if not(ser_state[0].lower() in m1[ser_name[1]]):
+				m1[ser_name[1]].append(ser_state[0].lower())							
+		else:
+			m1[ser_name[1]] = [ser_state[0].lower()]
+	else: 
+		break
+	
+output_file.close()
+
+output_file = open("/home/prasad/Desktop/btp/test2/service_data.pl","r")
+knowledge_base = output_file.readlines()
+output_file.close()
+
+in_file = open("/home/prasad/Desktop/btp/test2/service.txt","r")
+output_file = open("/home/prasad/Desktop/btp/test2/service_data.pl","w+")
+
+soft1 = "acpid"
+soft2 = "bluetooth"
+
+for line in in_file:
+	if (soft1 in line) or (soft2 in line):
+		a = line.split(' ')
+		ser_name = a[0].lower()
+		ser_state = a[1].lower()
+		if ser_name in m1:	
+			if not(ser_state in m1[ser_name]):
+				m1[ser_name].append(ser_state)
+				file_output = 'service_mon('+ser_name+','+ser_state+').\n'
+				output_file.write(file_output)			
+		else:	
+			m1[ser_name] = [ser_state]	
+			file_output = 'service_mon('+ser_name+','+ser_state+').\n'
+			output_file.write(file_output)
+
+output_file.writelines(knowledge_base)	
+output_file.close()
+in_file.close()	
+	    
+		
+
+
+
+
+
+
