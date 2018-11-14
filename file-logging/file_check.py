4e7fcba0378d65b315eae04d6336bab94e2a68b2
Prasad2098 <prasadparshallu2098@gmail.com>
2018-11-14 11:09:46 +0530

    Add files via upload

--- file-logging/file_check.py
+++ file-logging/file_check.py
@@ -7,21 +7,23 @@ username = getpass.getuser()
 m1 = {}
 m2 = {}
 
-output_file = open("/home/prasad/Desktop/btp/test2/file-logging/file_knowledgebase.txt","r")
+output_file = open("file_knowledgebase.txt","r")
 knowledge_base = output_file.readlines()
 output_file.close()
 
-in_file = open("/home/prasad/Desktop/btp/test2/file-logging/file_mon.txt","r")
-output_file = open("/home/prasad/Desktop/btp/test2/file-logging/file_data.pl","w+")
+in_file = open("file_mon.txt","r")
+output_file = open("file_data.pl","w+")
 
 file1 = "ssy"
 file2 = "/var/log/syslog"
 
 for line in in_file:
 	if ("ftrace_hook" in line) and ((file1 in line) or (file2 in line)):
-		a = line.split('ftrace_hook: ')
-		cmd = a[len(a)-1].split('(')
-				
+		a = line.split(' ')
+		cmd = a[4].split('(')
+		file_dir = a[4].split(',')
+		file_name = file_dir[1].split('/')
+		name = file_name[len(file_name)-1]		
 		if name in m1:	
 			if not(cmd[0] in m1[name]):
 				m1[name].append(cmd[0])
