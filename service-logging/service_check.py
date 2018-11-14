0aaa9d8e8a4df428761f72998c72bf737cfb9896
Prasad2098 <prasadparshallu2098@gmail.com>
2018-11-14 19:17:34 +0530

    Add files via upload

--- service-logging/service_check.py
+++ service-logging/service_check.py
@@ -1,33 +1,13 @@
 import os 
 import re
+import datetime
+
+currentDT = datetime.datetime.now()
 
 m1 = {}
 
-'''
-output_file = open("/home/prasad/Desktop/btp/test2/service-logging/service_data.pl","r")
-
-for line in output_file:
-	if 'service_mon' in line:
-		name = line.split(',')
-		ser_name = name[0].split('(')
-		ser_state = name[1].split(')')
-		if ser_name[1] in m1:		
-			if not(ser_state[0].lower() in m1[ser_name[1]]):
-				m1[ser_name[1]].append(ser_state[0].lower())							
-		else:
-			m1[ser_name[1]] = [ser_state[0].lower()]
-	else: 
-		break
-	
-output_file.close()
-'''
-
-output_file = open("service_knowledgebase.txt","r")
-knowledge_base = output_file.readlines()
-output_file.close()
-
 in_file = open("service.log","r")
-output_file = open("service_data.pl","w+")
+output_file = open("/home/prasad/Desktop/btp/en_file/service_data_"+str(currentDT.hour)+":"+str(currentDT.minute)+".pl","w+")
 
 soft1 = "acpid"
 soft2 = "bluetooth"
@@ -47,8 +27,6 @@ for line in in_file:
 			file_output = 'service_check('+ser_name+','+ser_state+',vm2).\n'
 			output_file.write(file_output)
 
-output_file.write('\n')
-output_file.writelines(knowledge_base)	
 output_file.close()
 in_file.close()	
 	    
